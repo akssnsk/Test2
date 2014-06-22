78aa5591938235a80a5bf2cd4e184b972a524da4
akssnsk <akss@outlook.com>
2014-06-22 11:23:55 -0700

    Guards fixed

--- TestApp/Task.h
+++ TestApp/Task.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef H_TASK_98BAA960_1BAD_465B_AFD0_A67E2ED1B2B3
+#define H_TASK_98BAA960_1BAD_465B_AFD0_A67E2ED1B2B3
 
 #ifdef __cplusplus
 extern "C" {
@@ -9,3 +10,5 @@ void removeComments(char *str);
 #ifdef __cplusplus
 }
 #endif // __cplusplus
+
+#endif // H_TASK_98BAA960_1BAD_465B_AFD0_A67E2ED1B2B3
